6e84f948efbc573d9e797aba2f679f18d3b222cc
Amadou Marong <marongamadou6@gmail.com>
2025-06-18 14:46:16 +0000

    added custom forms

---
+++ E-Commerce/server/accounts/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import CustomUser, UserProfile
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name')
+        
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+    
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label="Password", help_text="You can change the password using <a href=\"../password/\">this form</a>.")
+    
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+        
+    def clean_password(self):
+        return self.initial["password"]
+    
+    
+class UserProfileForm(forms.ModelForm):
+    model = UserProfile
+    fields = ('profile_picture', 'bio')
+    
+    # def clean_profile_picture(self):
+    #     profile_picture = self.cleaned_data.get('profile_picture')
+    #     if profile_picture and not profile_picture.name.lower().endswith(('.png', '.jpg', '.jpeg')):
+    #         raise forms.ValidationError("Only .png, .jpg, and .jpeg files are allowed.")
+    #     return profile_picture
