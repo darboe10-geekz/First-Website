60e9ab4c703f70c2f9a38ac8549c14023e2d3cd2
Amadou Marong <marongamadou6@gmail.com>
2025-06-18 14:59:01 +0000

    configured admin.py

--- E-Commerce/server/accounts/admin.py
+++ E-Commerce/server/accounts/admin.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
 from .models import CustomUser, UserProfile
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+# from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 
 
 class CustomUserAdmin(BaseUserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+    
+    list_display = ('email', 'first_name', 'last_name','is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'permissions')}),
+        # ('Important Dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    
     
     class Meta:
         model = CustomUser
