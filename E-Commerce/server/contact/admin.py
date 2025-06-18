5878040e58d466e6212f56c33c598c850aa00017
Amadou Marong <marongamadou6@gmail.com>
2025-06-18 10:12:00 +0000

    contact model

--- E-Commerce/server/contact/admin.py
+++ E-Commerce/server/contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+    list_filter = ('name', 'email')
+    search_fields = ('name', 'email')
+    ordering = ('name', 'email')
+    list_per_page = 10
+    list_max_show_all = 100
--- E-Commerce/server/contact/models.py
+++ E-Commerce/server/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    
+    def __str__(self):
+        return f"Contact: {self.name} - {self.email}"
+    
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+        
+        
