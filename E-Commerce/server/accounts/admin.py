9b8596f896c7b57720ac75826e665822d1e5ce6e
Amadou Marong <marongamadou6@gmail.com>
2025-06-18 14:52:20 +0000

    configured admin.py

--- E-Commerce/server/accounts/admin.py
+++ E-Commerce/server/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import CustomUser, UserProfile
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    
+    class Meta:
+        model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio']
+
+    class Meta:
+        model = UserProfile
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+
